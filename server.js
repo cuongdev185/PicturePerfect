f60b34feed481cd4c5b7c2348d575876b015aedb
cuongdev180503 <cuotie1852003@gmail.com>
2024-05-05 16:03:34 +0700

    TieCuong first commit

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('./src/app');
+const express = require('express');
 const PORT = process.env.SERVER_PORT || 5000
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!');
+})
 
 const server = app.listen (PORT, () => {
     console.log(`Server connected in port ${PORT} `)
